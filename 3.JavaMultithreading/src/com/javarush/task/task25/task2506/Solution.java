dbf03d6697bf34bc9373581175b32ade89d5d8d1
Veronika Sagina <sagina.veronika@yandex.ru>
2018-04-08 18:57:51 +0300

    Мониторинг состояния нити

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2506/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task25.task2506;
+
+/* 
+Мониторинг состояния нити
+*/
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        Thread target = new Thread(() -> {
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                //do nothing
+            }
+        });
+        LoggingStateThread loggingStateThread = new LoggingStateThread(target);
+
+        loggingStateThread.start(); //NEW
+        Thread.sleep(100);
+        target.start();  //RUNNABLE
+        Thread.sleep(100);
+        //TERMINATED
+    }
+}
+
